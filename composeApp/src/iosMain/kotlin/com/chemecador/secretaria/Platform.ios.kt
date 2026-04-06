02829590486085ceea9e4a0f8d39514ea5b97efb
chemecador <chemecador@gmail.com>
2026-04-06 22:57:33 +0200

    Initial commit

---
+++ composeApp/src/iosMain/kotlin/com/chemecador/secretaria/Platform.ios.kt
@@ -0,0 +1,9 @@
+package com.chemecador.secretaria
+
+import platform.UIKit.UIDevice
+
+class IOSPlatform: Platform {
+    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+}
+
+actual fun getPlatform(): Platform = IOSPlatform()
---
+++ composeApp/src/jvmMain/kotlin/com/chemecador/secretaria/main.kt
@@ -0,0 +1,13 @@
+package com.chemecador.secretaria
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Secretaria",
+    ) {
+        App()
+    }
+}
